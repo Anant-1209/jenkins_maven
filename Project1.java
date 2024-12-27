c73fb368ae22545a399078e46b71538a53343a02
ANANT KUMAR SAHU <109096647+Anant-1209@users.noreply.github.com>
2024-12-27 19:06:16 +0530

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,24 @@
+public class Project1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, SonarQube!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
